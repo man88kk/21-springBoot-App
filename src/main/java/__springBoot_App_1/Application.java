7876fbe7ec712bd7efbd9a0aede4d6d6422892e3
Manish Kumar <man88kk@gmail.com>
2025-05-12 00:50:05 +0530

    HIS- 134 fix provide

--- src/main/java/__springBoot_App_1/Application.java
+++ src/main/java/__springBoot_App_1/Application.java
@@ -10,6 +10,8 @@ public class Application {
 	public static void main(String[] args)
 	{
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		// changes for HIS-134
+
 		run.close();
 	}
 
